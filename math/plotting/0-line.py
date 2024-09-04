f366d9e9220d01b69fbcc66f63b8c166fd5e442a
Justice Chukwuonye <j.chukwuony@alustudent.com>
2024-09-04 20:18:03 +0000

    Scatter

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -7,4 +7,3 @@ y = np.arange(0, 11) ** 3
 plt.plot(y, 'r-')  # 'r-' specifies a red solid line
 plt.xlim(0, 10)  # Set x-axis range from 0 to 10
 plt.show()
-
---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,20 @@
+mport numpy as np
+import matplotlib.pyplot as plt
+
+mean = [69, 0]
+cov = [[15, 8], [8, 15]]
+np.random.seed(5)
+x, y = np.random.multivariate_normal(mean, cov, 2000).T
+y += 180
+
+# Plotting the data as a scatter plot
+plt.scatter(x, y, color='magenta')  # Plot data as magenta points
+
+# Adding labels and title
+plt.xlabel("Height (in)")
+plt.ylabel("Weight (lbs)")
+plt.title("Men's Height vs Weight")
+
+# Display the plot
+plt.show()
+
