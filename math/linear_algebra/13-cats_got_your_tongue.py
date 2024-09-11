85abbd72653a3d05a6051223c6c082617da20319
CeeJay <j.chukwuony@alustudent.com>
2024-09-11 15:43:22 +0100

    Update 13-cats_got_your_tongue.py

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -3,33 +3,27 @@ import numpy as np
 
 
 """
-Module for concatenating numpy ndarrays.
+Module for concatenating numpy ndarrays along a specified axis.
 
 Provides:
-    - np_cat(mat1, mat2, axis=0): Concatenates two arrays along a specified axis.
-
-Usage:
-    >>> import numpy as np
-    >>> mat1 = np.array([1, 2, 3])
-    >>> mat2 = np.array([4, 5, 6])
-    >>> np_cat(mat1, mat2)
-    array([1, 2, 3, 4, 5, 6])
+    - np_cat(mat1, mat2, axis=0)
 """
+
 
 def np_cat(mat1, mat2, axis=0):
     """
     Concatenate two numpy ndarrays along a specified axis.
 
     Args:
-        mat1 (numpy.ndarray): First array to concatenate.
-        mat2 (numpy.ndarray): Second array to concatenate.
+        mat1 (numpy.ndarray): First array.
+        mat2 (numpy.ndarray): Second array.
         axis (int, optional): Axis along which to concatenate. Default is 0.
 
     Returns:
         numpy.ndarray: Concatenated array.
 
     Raises:
-        ValueError: If the dimensions of mat1 and mat2 are not aligned along the specified axis.
+        ValueError
 
     Examples:
         >>> import numpy as np
@@ -37,29 +31,27 @@ def np_cat(mat1, mat2, axis=0):
         >>> mat2 = np.array([4, 5, 6])
         >>> np_cat(mat1, mat2)
         array([1, 2, 3, 4, 5, 6])
-
+        
         >>> mat1 = np.array([[1, 2], [3, 4]])
         >>> mat2 = np.array([[5, 6]])
         >>> np_cat(mat1, mat2, axis=0)
         array([[1, 2],
                [3, 4],
                [5, 6]])
-
+        
         >>> mat1 = np.array([[1, 2], [3, 4]])
         >>> mat2 = np.array([[5], [6]])
         >>> np_cat(mat1, mat2, axis=1)
         array([[1, 2, 5],
                [3, 4, 6]])
-
+        
         >>> mat1 = np.array([[[1], [2]], [[3], [4]]])
         >>> mat2 = np.array([[[5], [6]]])
         >>> np_cat(mat1, mat2, axis=0)
         array([[[1],
                [2]],
-
               [[3],
                [4]],
-
               [[5],
                [6]]])
     """
