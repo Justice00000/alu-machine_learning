49f41d4978d9c17e180d11ce7981a53030460efd
CeeJay <j.chukwuony@alustudent.com>
2024-09-11 09:50:00 +0100

    Update 3-flip_me_over.py

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 def matrix_transpose(matrix):
-    # Transpose the matrix using zip and unpacking
+    """
+    Transposes a 2D matrix.
+
+    Args:
+        matrix (list of lists): A 2D matrix (list of lists) to be transposed.
+
+    Returns:
+        list of lists: A new matrix that is the transpose of the input matrix.
+    """
     return [list(row) for row in zip(*matrix)]
