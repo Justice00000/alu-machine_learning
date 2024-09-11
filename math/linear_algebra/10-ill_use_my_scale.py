4bc3592a1f793bb98d2be852b91928dc32ee668b
CeeJay <j.chukwuony@alustudent.com>
2024-09-11 14:06:59 +0100

    Update 10-ill_use_my_scale.py

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
+
 def np_shape(matrix):
-    """Return the shape of the given numpy ndarray as a tuple of integers."""
-    if hasattr(matrix, 'shape'):
-        return matrix.shape
-    return tuple()  # Return an empty tuple if matrix does not have a shape attribute.
+    """
+    Recursively computes the shape of a matrix.
 
-# Testing the function
+    Args:
+        matrix (list): A list representing the matrix.
+
+    Returns:
+        tuple: A tuple of integers representing the shape of the matrix.
+    """
+    # Base cases when matrix is empty or non-list
+    return (len(matrix),) + np_shape(matrix[0])
+    if matrix and isinstance(matrix[0], list) else (len(matrix),)
+
+
+# Test cases to validate correctness
 if __name__ == "__main__":
-
-    mat1 = np.array([1, 2, 3, 4, 5, 6])
-    mat2 = np.array([])
-    mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
-                     [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+    mat1 = [1, 2, 3, 4, 5, 6]
+    mat2 = []
+    mat3 = [[1, 2, 3], [4, 5, 6]]
+    mat4 = [[[1], [2], [3]], [[4], [5], [6]], [[7], [8], [9]]]
+    
     print(np_shape(mat1))  # Output: (6,)
     print(np_shape(mat2))  # Output: (0,)
-    print(np_shape(mat3))  # Output: (2, 2, 5)
+    print(np_shape(mat3))  # Output: (2, 3)
+    print(np_shape(mat4))  # Output: (3, 3, 1)
