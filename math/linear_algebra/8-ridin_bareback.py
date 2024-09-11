6c89a2f312347a7f64b3bba3a3a8643e2b950d77
CeeJay <j.chukwuony@alustudent.com>
2024-09-11 11:34:54 +0100

    Update 8-ridin_bareback.py

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -14,10 +14,13 @@ def mat_mul(mat1, mat2):
 
     Returns:
         list of lists: The resulting matrix product.
-        If the matrices cannot be multiplied due to dimension mismatch.
+        None: If the matrices cannot be multiplied due to dimension mismatch.
     """
     if len(mat1[0]) != len(mat2):
         return None
 
-    result = [[sum(a * b for a, b in zip(row, col)) for col in zip(*mat2)] for row in mat1]
+    result = [
+        [sum(a * b for a, b in zip(row, col)) for col in zip(*mat2)]
+        for row in mat1
+    ]
     return result
