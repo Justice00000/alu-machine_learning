7f142eb34e57ae4604d5fdb5eb6f2bb11c4feea1
Justice <j.chukwuony@alustudent.com>
2024-09-11 01:17:06 -0700

    1-trim_me_down.py

---
+++ math/linear_algebra/1-trim_me_down.py
@@ -0,0 +1,4 @@
+x = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
+the_middle = [row[2:4] for row in matrix]  # Extract 3rd and 4th columns
+print("The middle columns of the matrix are: {}".format(the_middle))
+
