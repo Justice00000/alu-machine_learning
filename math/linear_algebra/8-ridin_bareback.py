2ada6eb2f3ad068c1a8c87067c130fac67bc4c71
CeeJay <j.chukwuony@alustudent.com>
2024-09-11 11:31:00 +0100

    Update 8-ridin_bareback.py

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 This module provides a function to perform matrix multiplication.
 """
@@ -18,7 +17,6 @@ def mat_mul(mat1, mat2):
     """
     if len(mat1[0]) != len(mat2):
         return None
-    
-    result = [[sum(a * b for a, b in zip(row, col)) 
-               for col in zip(*mat2)] for row in mat1]
+
+    result = [[sum(a * b for a, b in zip(row, col)) for col in zip(*mat2)] for row in mat1]
     return result
