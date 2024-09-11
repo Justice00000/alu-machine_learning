1736f5bd14b2325cd9090cf85c44eff50aadc3d7
CeeJay <j.chukwuony@alustudent.com>
2024-09-11 11:23:32 +0100

    Update 7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -11,11 +11,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
     Args:
         mat1 (list of lists): The first 2D matrix to concatenate.
         mat2 (list of lists): The second 2D matrix to concatenate.
-        axis (int, optional): The axis along which to concatenate. Defaults to 0.
+        axis (int, optional): The axis along which to concatenate.
 
     Returns:
         A new 2D matrix resulting from concatenation if dimensions match.
-        None: If the matrices cannot be concatenated due to mismatched dimensions.
+        If the matrices cannot be concatenated due to mismatched dimensions.
     """
     if axis == 0 and len(mat1[0]) == len(mat2[0]):
         return mat1 + mat2
