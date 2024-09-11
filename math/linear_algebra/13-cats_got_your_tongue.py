df78e9aae05def6b73a0f124e010493dc569624d
CeeJay <j.chukwuony@alustudent.com>
2024-09-11 15:40:56 +0100

    Update 13-cats_got_your_tongue.py

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -3,47 +3,18 @@ import numpy as np
 
 
 """
-Module for concatenating numpy ndarrays along a specified axis.
+Module for concatenating numpy ndarrays.
 
 Provides:
-    - np_cat(mat1, mat2, axis=0):
+    - np_cat(mat1, mat2, axis=0): Concatenates two arrays along a specified axis.
 
 Usage:
-    Import this module and use the np_cat function to concatenate arrays.
-
-    Examples:
-        >>> import numpy as np
-        >>> mat1 = np.array([1, 2, 3])
-        >>> mat2 = np.array([4, 5, 6])
-        >>> np_cat(mat1, mat2)
-        array([1, 2, 3, 4, 5, 6])
-
-        >>> mat1 = np.array([[1, 2], [3, 4]])
-        >>> mat2 = np.array([[5, 6]])
-        >>> np_cat(mat1, mat2, axis=0)
-        array([[1, 2],
-               [3, 4],
-               [5, 6]])
-
-        >>> mat1 = np.array([[1, 2], [3, 4]])
-        >>> mat2 = np.array([[5], [6]])
-        >>> np_cat(mat1, mat2, axis=1)
-        array([[1, 2, 5],
-               [3, 4, 6]])
-
-        >>> mat1 = np.array([[[1], [2]], [[3], [4]]])
-        >>> mat2 = np.array([[[5], [6]]])
-        >>> np_cat(mat1, mat2, axis=0)
-        array([[[1],
-               [2]],
-
-              [[3],
-               [4]],
-
-              [[5],
-               [6]]])
+    >>> import numpy as np
+    >>> mat1 = np.array([1, 2, 3])
+    >>> mat2 = np.array([4, 5, 6])
+    >>> np_cat(mat1, mat2)
+    array([1, 2, 3, 4, 5, 6])
 """
-
 
 def np_cat(mat1, mat2, axis=0):
     """
