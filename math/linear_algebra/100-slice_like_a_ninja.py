520e55dea2c2e1ba1e77e882d5640a46cccc4d91
Shobi <s.oadisa.dev@gmail.com>
2024-09-14 18:45:45 +0200

    update(fix): adv

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,11 +1,19 @@
-#!/usr/bin/env python3
 '''
-Module that slices a matrix along a specific axes
+Title: Slice like a Ninja | Python
 '''
-import numpy as np
+
 
 def np_slice(matrix, axes={}):
-    '''
-    Slices a matrix along a specific axes
-    '''
-    return np.s_[tuple([slice(*axes.get(i, (None, None))) for i in range(matrix.ndim)])]
+    """
+    Slices a matrix (nested list) along specific axes.
+    The slicing is similar to numpy slicing.
+    """
+    def apply_slice(data, slices, dim=0):
+        if not isinstance(data, list):
+            return data
+        if dim in slices:
+            start, stop, step = slices[dim]
+            data = data[start:stop:step]
+        return [apply_slice(item, slices, dim + 1) for item in data]
+
+    return apply_slice(matrix, axes)
