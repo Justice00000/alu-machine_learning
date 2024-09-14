da81f59523398cb082bd3171a1da33b89d723232
Shobi <s.oadisa.dev@gmail.com>
2024-09-14 19:04:37 +0200

    create(101): adv done

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 '''
-This is a simple script that demonstrates the use of the numpy library to solve a system of linear equations.
+This is a simple script that demonstrates the use
+of the numpy library to solve a system of linear equations. | Python | Shobi
 '''
-import numpy as np
 
 
 def add_matrices(mat1, mat2):
-    '''
-    Adds two matrices together
-    '''
-    if np.shape(mat1) != np.shape(mat2):
+    if type(mat1) != type(mat2):
         return None
-    sum_mat = np.add(mat1, mat2)
-    return sum_mat
+
+    if isinstance(mat1, (int, float)):
+        return mat1 + mat2
+
+    if len(mat1) != len(mat2):
+        return None
+
+    result = []
+
+    for i in range(len(mat1)):
+        if isinstance(mat1[i], list):
+            sub_result = add_matrices(mat1[i], mat2[i])
+            if sub_result is None:
+                return None
+            result.append(sub_result)
+        elif isinstance(mat1[i], (int, float)):
+            result.append(mat1[i] + mat2[i])
+        else:
+            return None
+
+    return result
