603fe43c771f635a149f96d6db5b15be30ca1073
Shobi <s.oadisa.dev@gmail.com>
2024-09-14 18:54:03 +0200

    update(100): corrects formatting

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -8,5 +8,8 @@ def np_slice(matrix, axes={}):
     """
     Slices a matrix (numpy.ndarray) along specific axes.
     """
-    slices = tuple(slice(*axes.get(i, (None, None, None))) for i in range(matrix.ndim))
+    slices = tuple(
+        slice(*axes.get(i, (None, None, None)))
+        for i in range(matrix.ndim)
+    )
     return matrix[slices]
