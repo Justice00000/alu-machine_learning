744aee713c6872e48adb1443cb7239b1dd826e63
Shobi <s.oadisa.dev@gmail.com>
2024-09-14 18:49:12 +0200

    update(100): corrects return type

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python3
 '''
-Title: Slice like a Ninja | Python
+Title: Slice like a Ninja | Python | Shobi
 '''
 
 
 def np_slice(matrix, axes={}):
     """
-    Slices a matrix (nested list) along specific axes.
-    The slicing is similar to numpy slicing.
+    Slices a matrix (numpy.ndarray) along specific axes.
     """
-    def apply_slice(data, slices, dim=0):
-        if not isinstance(data, list):
-            return data
-        if dim in slices:
-            start, stop, step = slices[dim]
-            data = data[start:stop:step]
-        return [apply_slice(item, slices, dim + 1) for item in data]
-
-    return apply_slice(matrix, axes)
+    slices = tuple(slice(*axes.get(i, (None, None, None))) for i in range(matrix.ndim))
+    return matrix[slices]
