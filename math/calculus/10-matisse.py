885b91ac7ff1a90f27083fac823563a14887c4ca
Shobi <s.oadisa.dev@gmail.com>
2024-10-01 08:22:47 +0200

    fixes

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''
-Write a function that takes a list of integers as input and returns the derivative of the polynomial represented by the list.
+Write a function that takes a list of integers as input
+and returns the derivative of the polynomial represented by the list.
 '''
 
 
@@ -8,7 +9,17 @@ def poly_derivative(poly):
     '''
     Returns the derivative of the polynomial represented by the list poly.
     '''
-    if len(poly) < 2:
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+
+    if len(poly) == 1:
         return [0]
-    
-    return [poly[i] * i for i in range(1, len(poly))]
+
+    derivative = []
+    for power, coeff in enumerate(poly[1:], start=1):
+        derivative.append(coeff * power)
+
+    while len(derivative) > 1 and derivative[-1] == 0:
+        derivative.pop()
+
+    return derivative if any(derivative) else [0]
