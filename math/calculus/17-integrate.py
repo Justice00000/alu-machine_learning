f101044cab8c78c5b0bdbff2b5a5d6c991599e93
CeeJay <j.chukwuony@alustudent.com>
2024-10-12 18:22:10 +0100

    Update 17-integrate.py

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 """_summary_
 This file contains the implementation of poly_integral
@@ -27,33 +26,3 @@ def poly_integral(poly, C=0):
     while integrals[-1] == 0 and len(integrals) > 1:
         integrals.pop()
     return integrals
-=======
-#!/usr/bin/env python3
-"""_summary_
-This file contains the implementation of poly_integral
-"""
-
-
-def poly_integral(poly, C=0):
-    """_summary_
-    Computes the coefficients of the terms in the
-    integral of a function using Sum rule
-    """
-    if not isinstance(poly, list) or not isinstance(C, (int, float)) or not poly:  # noqa
-        return None
-    if not all(isinstance(c, (int, float)) for c in poly):
-        return None
-
-    integrals = [C]
-    for power, coefficient in enumerate(poly):
-        if power == 0:
-            integrals.append(coefficient)
-        else:
-            integral = coefficient / (power + 1)
-            integrals.append(
-                int(integral) if integral.is_integer() else integral
-            )
-    while integrals[-1] == 0 and len(integrals) > 1:
-        integrals.pop()
-    return integrals
->>>>>>> a262473352dd582d190fed95fa6e5c665f37f9ba
