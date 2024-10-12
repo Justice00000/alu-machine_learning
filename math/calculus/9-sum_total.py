943db6f463b353e3ce4d9a175bc41f12eef20d1d
CeeJay <j.chukwuony@alustudent.com>
2024-10-12 18:26:25 +0100

    Update 9-sum_total.py

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 """
 This file contains the implementation of summation_i_squared
@@ -20,26 +19,3 @@ def summation_i_squared(n):
     #     return 1
 
     # return n**2 + summation_i_squared(n-1)
-=======
-#!/usr/bin/env python3
-"""
-This file contains the implementation of summation_i_squared
-"""
-
-
-def summation_i_squared(n):
-    """_summary_
-    A function that computes the result a sigma notation.
-    """
-    if not isinstance(n, int) or n < 1:
-        return None
-
-    # Using the mathematical formular to computes the sum of squares
-    return n * (n + 1) * (2 * n + 1) // 6
-
-    # Recursive approach (Not suitable when n is large)
-    # if n == 1:
-    #     return 1
-
-    # return n**2 + summation_i_squared(n-1)
->>>>>>> a262473352dd582d190fed95fa6e5c665f37f9ba
