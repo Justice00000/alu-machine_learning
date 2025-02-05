0f55b5f051556af98d1724b4ec9d547532f06a87
Shobi <apple@Shobis-MacBook-Pro.local>
2025-02-05 16:01:22 +0200

    Update: adhere to pycodestle

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -2,9 +2,11 @@
 
 import numpy as np
 
+
 def l2_reg_cost(cost, lambtha, weights, L, m):
     '''
-    Function that calculates the cost of a neural network with L2 regularization
+    Function that calculates the cost of a neural
+    network with L2 regularization
 
     Parameters:
     cost: float - cost of the network without L2 regularization
@@ -13,16 +15,11 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     L: int - number of layers in the network
     m: int - number of data points used
     '''
-    # Initialize sum of squared weights
     weights_squared_sum = 0
-    
-    # Sum the squared Frobenius norms of all weight matrices
+
     for i in range(L):
-        W = weights[f'W{i+1}']
+        W = weights['W' + str(i+1)]
         weights_squared_sum += np.sum(np.square(W))
-    
-    # Calculate L2 regularization term
+
     l2_cost = (lambtha / (2 * m)) * weights_squared_sum
-    
-    # Return total cost: original cost + L2 regularization term
-    return float(cost + l2_cost)
+    return np.array([float(cost + l2_cost)])
